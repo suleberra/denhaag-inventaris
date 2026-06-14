092ba85a72d026aca9d85200e15cf264b69c0c50
Berra Kilic <kberrasule@gmail.com>
2026-06-14 15:57:21 +0200

    Display trees layer

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
+
+fetch('data/processed/bomen_processed.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data).addTo(map);
+    });
